a14deab1e2019d14af7c5ee6bda3447bb300d307
olgeradriano <olgeradrianomeza@gmail.com>
2025-08-26 13:36:15 -0500

    cambiosv1

--- ms-catalogo/src/main/java/com/example/mscatalogo/Respository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mscatalogo.Respository;
-
-import com.example.mscatalogo.Entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository  extends JpaRepository<Categoria,Integer> {
-}
